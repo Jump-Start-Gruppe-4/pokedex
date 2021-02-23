9a9c13f8b3ffc4c0fb593e1441278a86f4bb7ed2
oskarbeilfuss <oskar.beilfuss@accenture.com>
2021-02-23 15:48:26 +0100

    JavaScript Anzeige korrekt

--- app.js
+++ app.js
@@ -11,23 +11,30 @@ formEl.addEventListener("submit", (e) => {
   getPokemon(inputEl.value);
 });
 
-async function getPokemon(name = "charmander") {
+async function getPokemon(name) {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   const pokemon = await res.json();
+
 
   const pokemonEl = document.createElement("div");
   pokemonEl.classList.add("pokemon");
 
   pokemonEl.innerHTML = `
     <div class="info">
-      <img src="https://pokeres.bastionbot.org/images/pokemon/${
-        pokemon.id
-      }.png" width="200">
+      <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" width="200">
 <h2>${pokemon.name}</h2>
+<h3>Type       ${pokemon.types.map((type) => type.type.name).join(', ')}</h3>
+<h3>Abilities: ${pokemon.abilities.map((ability) => ability.ability.name).join(', ')}</h3>
 
+<div class="stats">${pokemon.stats.map((stat) => {
+    return `<p>${stat.stat.name}: ${stat.base_stat}</p>`;}).join("")}</div>
 `;
+
+
+
 
 pokemonContainer.appendChild(pokemonEl);
 }
 
 getPokemon();
+
