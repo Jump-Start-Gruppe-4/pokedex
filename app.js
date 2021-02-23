ad9270870a421c18f6d21f44b587e15a7c39f7cc
oskarbeilfuss <oskar.beilfuss@accenture.com>
2021-02-23 14:06:28 +0100

    0.1

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 
 
-function fetchPokemon() {
-    var id = 1
+//Imported Code
+const pokemonContainer = document.querySelector(".pokemon-container");
+const formEl = document.querySelector("form");
+const inputEl = document.querySelector("input[type=text]");
 
-    const url = 'https://pokeapi.co/api/v2/pokemon/1';
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  pokemonContainer.innerHTML = "";
+  getPokemon(inputEl.value);
+});
 
-    fetch(url)
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data);
-            const pokemon = {};
-            pokemon['name'] = data.name;
-            pokemon['id'] = data.id;
-            pokemon['image'] = data.sprites['front_default'];
-            pokemon['type'] = data.types.map((type) => type.type.name).join(', ');
+async function getPokemon(name = "charmander") {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const pokemon = await res.json();
 
-            console.log(pokemon);
-        })
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
 
+  pokemonEl.innerHTML = `
+    <div class="info">
+      <img src="https://pokeres.bastionbot.org/images/pokemon/${
+        pokemon.id
+      }.png" width="200">
+<h2>${pokemon.name}</h2>
 
+`;
+
+pokemonContainer.appendChild(pokemonEl);
 }
 
-fetchPokemon();
+getPokemon();
